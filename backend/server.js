9f6f4efc5c9af42d3514de0da380da6fbcaf301d
Moffat <moffat.muga@gmail.com>
2024-03-07 21:11:44 +0300

    add connectios

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const colors = require('colors')
+const mongoose = require('mongoose')
 const dotenv = require('dotenv').config()
 const port =  process.env.PORT || 5000
 
+
+
+
+
 const app = express()
+mongoose.connect("mongodb+srv://moffat123:moffat123@moffatcluster.0s4guzy.mongodb.net/goals-app?retryWrites=true&w=majority&appName=MoffatCluster")
+    .then(() => {
+        console.log('connected to database!')
+        app.listen(port, () => console.log(`server started on port ${port}`))
+    })
+    .catch(() => {
+        console.log('connection failed')
+    })
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.listen(port, () => console.log(`server started on port ${port}`))
+
+
 
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use(errorHandler)
